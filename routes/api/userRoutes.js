e3bed8359760575b6f2e05cf67d0a92d004e2914
OGrunner <orshagoines@gmail.com>
2022-10-12 06:39:39 -0400

    routes created

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+// user routes
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
+
+module.exports = router;
